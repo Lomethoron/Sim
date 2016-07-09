9bd8ccbfe60e003dacef9475032df8f7235a2a2c
lomethoron <wdixon23192@gmail.com>
2016-07-09 17:04:09 -0400

    Added empty buisness class, started fleshing out person.

--- Assets/Person.cs
+++ Assets/Person.cs
@@ -1,14 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum AgeType { Infant, Child, Adult, Senior};
+
 public class Person : MonoBehaviour {
 
     public int money;
     public int age;
+    public AgeType ageType;
+
+    //temp to test simulation
+    public int incomePerMonth;
     
 
 	// Use this for initialization
-	void Start () {
+	void Start (AgeType ageType) {
+        this.ageType = ageType;
+
+        switch (ageType) {
+            case AgeType.Infant: {
+                    incomePerMonth = 0;
+                    break;
+                }
+            case AgeType.Child: {
+                    incomePerMonth = 20;
+                    break;
+                }
+            case AgeType.Adult: {
+                    incomePerMonth = 5000;
+                    break;
+                }
+            case AgeType.Senior: {
+                    incomePerMonth = 500;
+                    break;
+                }
+            default: {
+                    throw new System.Exception("Person has no specified Age Type!");
+                }
+        }
 	
 	}
 	
